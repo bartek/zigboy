8afcf25117a15ab05fc824b4400c62af41a2cf98
Bartek Ciszkowski <bartek@wattpad.com>
2021-08-14 21:57:47 -0300

    Introduce main loop

--- src/cpu.zig
+++ src/cpu.zig
@@ -1,8 +1,9 @@
 const std = @import("std");
+const print = std.debug.print;
 const Allocator = std.mem.Allocator;
 
 const register = struct {
-    value: uint16,
+    value: u16,
 
     pub fn setLo(self: *register, value: u8) void {
         self.value = u16(value) | u16(self.value)&0xFF00;
@@ -13,15 +14,49 @@ const register = struct {
     }
 };
 
-// The Game Boy CPU is composed of 8 different registers which are responsible
+
+pub const Memory = struct {
+    // GameBoy contains an 8-bit processor, meaning it can access 8-bits of data
+    // at one time. To access this data, it has a 16-bit address bus, which can
+    // address 65,536 positions of memory.
+    const memory_size = 65536;
+
+    allocator: *Allocator,
+    memory: []u8,
+
+    pub fn init(allocator: *Allocator) !Memory {
+        return Memory{
+            .allocator = allocator,
+            .memory = try allocator.alloc(u8, memory_size),
+        };
+    }
+
+    pub fn deinit(self: *Memory) void {
+        self.allocator.free(self.memory);
+    }
+
+    pub fn read(self: *Memory, address: u16) u8 {
+        return self.memory[address];
+    }
+
+    pub fn write(self: *Memory, address: u16, value: u8) void {
+        self.memory[address] = value;
+    }
+
+    pub fn loadRom(self: *Memory, buffer: []u8) !void {
+        for (buffer) |b, index| {
+            self.write(@intCast(u16, index), b);
+        }
+    }
+};
+
+// The GameBoy CPU is composed of 8 different registers which are responsible
 // for holding onto little pieces of data that the CPU can manipulate when it
 // executes various instructions. These registers are named A, B, C, D, E, F, H,
 // and L. Since they are 8-bit registers, they can hold only 8-bit values.
-// However, the Game Boy can combine two registers in order to read and write
+// However, the GameBoy can combine two registers in order to read and write
 // 16-bit values. The valid combinations then are AF, BC, DE, and HL.
 pub const CPU = struct {
-    allocator: *Allocator,
-
     af: register,
     bc: register,
     de: register,
@@ -33,8 +68,48 @@ pub const CPU = struct {
     // Stack Pointer
     sp: u16,
 
+    memory: Memory,
+
     pub fn init(allocator: *Allocator) !CPU {
         return CPU{
+            .memory = try Memory.init(allocator),
+            .af = undefined,
+            .bc = undefined,
+            .de = undefined,
+            .hl = undefined,
+            .pc = 0x00,
+            .sp = undefined,
         };
     }
+
+    pub fn deinit(self: *CPU) void {
+        self.memory.deinit();
+    }
+
+    // tick ticks the CPU
+    pub fn tick(self: *CPU) void{ 
+        var opcode = self.fetch();
+        self.execute(opcode);
+    }
+
+    pub fn fetch(self: *CPU) u16 {
+        // read from memory
+        // increment pc
+        // return opcode
+        var opcode: u16 = self.memory.read(self.pc);
+        self.pc += 1;
+        return opcode;
+    }
+
+
+    pub fn execute(self: *CPU, opcode: u16) void {
+        print("0x{x}", .{opcode});
+        // execute opcode
+    }
 };
+
+test "CPU" {
+    var cpu = try CPU.init(std.heap.page_allocator);
+    cpu.tick();
+    cpu.deinit();
+}
--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,22 @@
 const std = @import("std");
+const fs = std.fs;
+const cwd = fs.cwd();
+const warn = std.log.warn;
+
+const c = @import("./cpu.zig");
 
 pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
+    const allocator = std.heap.page_allocator;
+
+    var cpu = try c.CPU.init(allocator);
+     
+    // Read the boot ROM into memory
+    const buffer = cwd.readFileAlloc(allocator, "./roms/DMG_ROM.bin", 256) catch |err| {
+        warn("unable to open file: {s}\n", .{@errorName(err)});
+        return err;
+    };
+
+    try cpu.memory.loadRom(buffer);
+
+    cpu.tick();
 }
