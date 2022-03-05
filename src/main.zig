cbdae1dead9633ec4f1427c4a15e3d92cef0b173
Bartek Ciszkowski <hello@justbartek.ca>
2022-03-04 21:41:17 -0400

    Naive debug arg parsing

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,10 @@
 const std = @import("std");
-const eql = std.mem.eql;
-const print = std.debug.print;
-const fs = std.fs;
+const clap = @import("clap");
+
 const cwd = fs.cwd();
+const eql = std.mem.eql;
+const fs = std.fs;
+const print = std.debug.print;
 const warn = std.log.warn;
 
 const stdin = std.io.getStdIn().reader();
@@ -12,6 +14,20 @@ const State = @import("./state.zig").State;
 
 pub fn main() anyerror!void {
     const allocator = std.heap.page_allocator;
+
+    var debug: bool = false;
+
+    // Identify args
+    var args = std.process.args();
+    while (args.next(allocator)) |error_or_arg| {
+        const arg = error_or_arg catch |err| {
+            warn("Error parsing arguments: {s}", .{err});
+            return err;
+        };
+        if (eql(u8, arg, "-d")) {
+            debug = true;
+        }
+    }
 
     var cpu = try c.CPU.init(allocator);
 
@@ -55,15 +71,17 @@ pub fn main() anyerror!void {
         var line = line_buffer.items;
 
         // Pause when state differs from bootromlog
-        if (!eql(u8, state.top(), line_buffer.items)) {
-            quickSleep();
-            print("\n\n", .{});
-            for (state.current()) |l, index| {
-                print("{d}\t{s}\t{s}\n", .{ i, l, state.instructions.items[index] });
-            }
+        if (debug) {
+            if (!eql(u8, state.top(), line_buffer.items)) {
+                quickSleep();
+                print("\n\n", .{});
+                for (state.current()) |l, index| {
+                    print("{d}\t{s}\t{s}\n", .{ i, l, state.instructions.items[index] });
+                }
 
-            print("+\t{s}\n", .{line});
-            print("\n\n", .{});
+                print("+\t{s}\n", .{line});
+                print("\n\n", .{});
+            }
         }
     }
 }
